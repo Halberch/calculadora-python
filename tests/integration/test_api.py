a094e4ed2e141e9a060793bde1e91deb5b9603c1
Halberch <albertogc2211@gmail.com>
2026-02-09 20:19:17 +0100

    Base funcional: Suma y Resta con Tests Q1 y Q2 pasando

---
+++ tests/integration/test_api.py
@@ -0,0 +1,20 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_api_add(client):
+    response = client.post('/add', json={'a': 10, 'b': 20})
+    data = response.get_json()
+    assert response.status_code == 200
+    assert data['result'] == 30
+
+def test_api_subtract(client):
+    response = client.post('/subtract', json={'a': 10, 'b': 4})
+    data = response.get_json()
+    assert response.status_code == 200
+    assert data['result'] == 6
