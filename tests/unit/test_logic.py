741fde158d1333309540ec0632794b9fdb5cf092
Marta García <martagarv@gmail.com>
2026-02-12 22:40:16 +0100

    Add multipy and divide with their tests

--- tests/unit/test_logic.py
+++ tests/unit/test_logic.py
@@ -1,4 +1,4 @@
-from logic import add, subtract
+from logic import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 2) == 4
@@ -7,3 +7,12 @@ def test_add():
 def test_subtract():
     assert subtract(10, 5) == 5
     assert subtract(5, 10) == -5
+
+def test_multiply():
+    assert multiply(3, 4) == 12
+    assert multiply(-2, 5) == -10
+
+def test_divide():
+    assert divide(10, 2) == 5
+    assert divide(-10, 2) == -5
+    assert divide(10, -2) == -5
